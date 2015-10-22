00bb6743a4bc93274908a86fb2e5b3f5bc90a988
jlee126 <jlee126@pratt.edu>
2015-10-22 02:47:50 +0000

    sfa

---
+++ cheese/js/script.js
@@ -0,0 +1,22 @@
+//var random_images = ["sushi1.png", "sushi2.png", "sushi3.png", "sushi4.png", "sushi5.png", "sushi6.png", "sushi7.png", "sushi8.png", "sushi9.png"];
+
+//console.log("its working")
+
+
+//$(".toggle").click(function() {
+
+
+  //  $(".images").each(function() {
+    //    var randomimg = random_images[Math.floor(Math.random()*random_images.length)];
+       // $(this).html("<img src='img/"+ randomimg + "'>");
+   // });
+//});
+
+//$(".toggle").click(function() {
+
+
+   // $(".sushi").each(function() {
+    //    var randomimg = random_images[Math.floor(Math.random()*random_images.length)];
+     //   $(this).html("<img src='img/"+ randomimg + "'>");
+  //  });
+// });
