20205c047fdb774506ae80470111b4c7498ca174
jlee126 <jlee126@pratt.edu>
2016-04-19 05:05:58 +0000

    new

--- haiku/javas/script.js
+++ haiku/javas/script.js
@@ -1,6 +1,7 @@
 
 var random_images = ["haiku1.png", "haiku2.png", "haiku3.png", "haiku4.png", "haiku5.png", "haiku6.png", "haiku7.png", "haiku8.png", "haiku9.png",
-"haiku10.png", "haiku11.png", "haiku12.png", "haiku13.png", "haiku14.png", "haiku15.png"];
+"haiku10.png", "haiku11.png", "haiku12.png", "haiku13.png", "haiku14.png", "haiku15.png", "haiku16.png", "haiku17.png",
+"haiku18.png", "haiku19.png", "haiku20.png", "haiku21.png", "haiku22.png", "haiku23.png"];
 
 
 
@@ -17,7 +18,9 @@ $(".toggle").click(function() {
 });
 
 var random__images = ["puppies.jpg", "alpacasso.jpg", "panda.JPG", "cinnabon.jpg", "innout.jpg", "tacoshop.jpeg", "cacti.jpg",
-"cactusman.jpg", "minnie1.jpg", "minnie2.jpg", "kimchi.jpg", "IMG_0566.JPG", "alpacasso2.jpg"]
+"cactusman.jpg", "minnie1.jpg", "minnie2.jpg", "kimchi.jpg", "IMG_0566.JPG", "alpacasso2.jpg", "hopkins.jpg", "flooffycow.jpg", "polaroid.jpg",
+"blank.jpg", "prattcat.jpg", "hopkins.jpg", "jellyfish.jpg", "painting.jpg", "bed.jpg", "rubios.jpg", "usher.jpg", "toilet.jpg", "99ranch.jpg",
+]
 $(".toggle").click(function() {
 
 
